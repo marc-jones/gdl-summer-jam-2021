8d43a747b66e23a500a9a4198ce350c133192c13
Marc Jones <m4rcj0n35@gmail.com>
2021-08-29 01:49:56 +0100

    Zhoozhed up the menu

--- scripts/MenuState.gd
+++ scripts/MenuState.gd
@@ -5,3 +5,8 @@ signal start_game
 func _ready():
 	$Buttons/HBox/Start.connect(
 		"button_up", self, "emit_signal", ["start_game"])
+
+func _process(_delta):
+	$Controls/FreezeIndicator.update_sprites(
+		clamp($FreezeTimer.get_time_left()-1.0, 0.0, 6.0)
+	)
